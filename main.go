e60713be0b6c3d8567e6fe9b0e7db771ae292acd
M. Solodov <solodov.mn@gmail.com>
2026-07-05 15:21:16 +0300

    Базовая часть

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type User struct {
 	ID   int
@@ -8,40 +11,94 @@ type User struct {
 }
 
 type Graph struct {
-	// TODO: добавьте поля для хранения пользователей и связей
+	mu sync.Mutex
+
+	users       map[int]*User
+	connections map[int][]int
 }
 
 func NewGraph() *Graph {
-	// TODO
-	return nil
+	return &Graph{
+		users:       make(map[int]*User),
+		connections: make(map[int][]int),
+	}
 }
 
 func (g *Graph) AddUser(id int, name string) {
-	// TODO
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	g.users[id] = &User{
+		ID:   id,
+		Name: name,
+	}
 }
 
 func (g *Graph) GetUser(id int) (*User, bool) {
-	// TODO
-	return nil, false
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	user, ok := g.users[id]
+	return user, ok
 }
 
 func (g *Graph) AddConnection(fromID, toID int) bool {
-	// TODO: вернуть false если один из пользователей не существует
-	return false
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	_, fromOk := g.users[fromID]
+	_, toOk := g.users[toID]
+	if !fromOk || !toOk || fromID == toID {
+		return false
+	}
+
+	g.connections[fromID] = append(g.connections[fromID], toID)
+	g.connections[toID] = append(g.connections[toID], fromID)
+
+	return true
 }
 
 func (g *Graph) GetConnections(userID int) []*User {
-	// TODO: вернуть слайс указателей на пользователей
-	return nil
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	if len(g.connections[userID]) == 0 {
+		return nil
+	}
+
+	cUsers := make([]*User, 0, len(g.connections[userID]))
+
+	for _, id := range g.connections[userID] {
+		cUsers = append(cUsers, g.users[id])
+	}
+
+	return cUsers
 }
 
 func (g *Graph) HasConnection(fromID, toID int) bool {
-	// TODO
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	if len(g.connections[fromID]) == 0 || len(g.connections[toID]) == 0 || fromID == toID {
+		return false
+	}
+
+	//микрооптимизация - ищем по меньшему массиву
+	if len(g.connections[fromID]) > len(g.connections[toID]) {
+		fromID, toID = toID, fromID
+	}
+
+	for _, id := range g.connections[fromID] {
+		if id == toID {
+			return true
+		}
+	}
+
 	return false
 }
 
 func (g *Graph) UserCount() int {
-	return 0
+	return len(g.users)
 }
 
 func main() {
